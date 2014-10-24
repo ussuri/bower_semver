0241d0a89b9852f52e490ea6e17e7088fbac1dd6
Sergey Shevchenko <sergeygs@google.com>
2014-10-24 15:24:35 -0700

    Add new licenses to changed sources + change pub package name to avoid potential conflicts

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 library pub_semver.src.version;
 
 import 'dart:math';
